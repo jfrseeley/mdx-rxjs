7ea0892a961d4200afeaaf7705e198140428d557
Jeffrey Seeley <jeffrey.seeley@healthcatalyst.com>
2019-11-08 14:12:49 -0700

    Created bare bones test harness.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent]
     }).compileComponents();
   }));
 
@@ -14,18 +12,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'mdx-rxjs'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('mdx-rxjs');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('mdx-rxjs app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Mdx, SoapMdxHandler } from '../../projects/mdx-rxjs/src';
+
+interface IMdxFormData {
+  cube: string;
+  catalog: string;
+  url: string;
+}
+
+interface IChartFormData {
+  measures: string;
+  xAxis: string;
+  groupBy?: string;
+}
+
+interface IDimensionFormData {
+  attributes: string;
+  measures?: string;
+}
+
+interface ITableRowFormData {
+  measures: string;
+  rows: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +30,90 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mdx-rxjs';
+  mdxForm: FormGroup;
+  chartForm: FormGroup;
+  dimensionForm: FormGroup;
+  tableRowForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.mdxForm = this.formBuilder.group({
+      cube: 'Model',
+      catalog: 'Test',
+      url: 'http://localhost/olap/'
+    });
+
+    this.chartForm = this.formBuilder.group({
+      measures: ['', Validators.required],
+      xAxis: ['', Validators.required],
+      groupBy: ['']
+    });
+    this.dimensionForm = this.formBuilder.group({
+      attributes: ['', Validators.required],
+      measures: ['']
+    });
+    this.tableRowForm = this.formBuilder.group({
+      measures: ['', Validators.required],
+      rows: ['', Validators.required]
+    });
+  }
+
+  loadChart() {
+    const data: IChartFormData = this.chartForm.value;
+    const measures = data.measures.split('\n');
+
+    this.createMdx()
+      .getChartData({
+        measures,
+        xAxisLevelExpression: data.xAxis,
+        groupByLevelExpression: data.groupBy
+      })
+      .subscribe(
+        chart => {
+          console.log(chart);
+        },
+        error => {
+          console.error(error);
+        }
+      );
+  }
+
+  loadDimension() {
+    const data: IDimensionFormData = this.dimensionForm.value;
+    const attributes = data.attributes.split('\n');
+    const measures = data.measures ? data.measures.split('\n') : undefined;
+
+    this.createMdx()
+      .getDimensionData(attributes, {
+        measures
+      })
+      .subscribe(
+        result => {
+          console.log(result);
+        },
+        error => {
+          console.error(error);
+        }
+      );
+  }
+
+  loadTableRows() {
+    const data: ITableRowFormData = this.tableRowForm.value;
+    const measures = data.measures.split('\n');
+    const rows = data.rows.split('\n');
+    this.createMdx()
+      .getTableRowData(measures, rows)
+      .subscribe(
+        result => {
+          console.log(result);
+        },
+        error => {
+          console.error(error);
+        }
+      );
+  }
+
+  private createMdx(): Mdx {
+    const data: IMdxFormData = this.mdxForm.value;
+    return new Mdx(data.cube, new SoapMdxHandler(data.catalog, data.url));
+  }
 }
